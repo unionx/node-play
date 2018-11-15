c291e3dcba1f30ab2303a0f3bc3cf077a8589977
unionx <unionx@foxmail.com>
2018-11-16 05:49:50 +0800

    Add unit tests with mocha

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello, Node.');
+  res.send('Hello, Node.');
 });
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000');
+  console.log('Example app listening on port 3000');
 });
